98e78da099e789674b3a886913d2f0c5b8fe9fe9
Joel Chi <abongsjoel@gmail.com>
2025-10-31 23:06:54 +0100

    Remove “Create a space” button

--- src/components/views/spaces/SpacePanel.tsx
+++ src/components/views/spaces/SpacePanel.tsx
@@ -224,6 +224,7 @@ const CreateSpaceButton: React.FC<Pick<IInnerSpacePanelProps, "isPanelCollapsed"
     setPanelCollapsed,
 }) => {
     const [menuDisplayed, handle, openMenu, closeMenu] = useContextMenu<HTMLDivElement>();
+    // const [menuDisplayed, handle, openMenu, closeMenu] = useContextMenu<HTMLDivElement>();
 
     useEffect(() => {
         if (!isPanelCollapsed && menuDisplayed) {
@@ -236,12 +237,12 @@ const CreateSpaceButton: React.FC<Pick<IInnerSpacePanelProps, "isPanelCollapsed"
         contextMenu = <SpaceCreateMenu onFinished={closeMenu} />;
     }
 
-    const onNewClick = menuDisplayed
-        ? closeMenu
-        : () => {
-              if (!isPanelCollapsed) setPanelCollapsed(true);
-              openMenu();
-          };
+    // const onNewClick = menuDisplayed
+    //     ? closeMenu
+    //     : () => {
+    //           if (!isPanelCollapsed) setPanelCollapsed(true);
+    //           openMenu();
+    //       };
 
     return (
         <li
@@ -251,7 +252,7 @@ const CreateSpaceButton: React.FC<Pick<IInnerSpacePanelProps, "isPanelCollapsed"
             role="treeitem"
             aria-selected={false}
         >
-            <SpaceButton
+            {/* <SpaceButton
                 data-testid="create-space-button"
                 className={classNames("mx_SpaceButton_new", {
                     mx_SpaceButton_newCancel: menuDisplayed,
@@ -261,7 +262,7 @@ const CreateSpaceButton: React.FC<Pick<IInnerSpacePanelProps, "isPanelCollapsed"
                 isNarrow={isPanelCollapsed}
                 innerRef={handle}
                 size="32px"
-            />
+            /> */}
 
             {contextMenu}
         </li>
